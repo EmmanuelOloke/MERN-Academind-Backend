2f505fb57d844dae6e5eff06da81dd8f395e5336
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-06-02 14:30:55 +0100

    added updated and delete place middlewares

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -10,4 +10,8 @@ router.get('/user/:uid', placesControllers.getPlaceByUserId);
 
 router.post('/', placesControllers.createPlace);
 
+router.patch('/:pid', placesControllers.updatePlace);
+
+router.delete('/:pid', placesControllers.deletePlace);
+
 module.exports = router; // How to export in Nodejs i.e What is being exported in the file is the router constant
