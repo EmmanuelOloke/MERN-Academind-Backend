a2566a81dfd9eb0574284374174c97710bca79d5
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-06-02 14:38:51 +0100

    fiexd bug with getPlaceByUserId to accomodate for single user creating multiple places instead as getPlacesByUserId

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -6,7 +6,7 @@ const router = express.Router(); // Gives us a special object on which we can re
 
 router.get('/:pid', placesControllers.getPlaceById);
 
-router.get('/user/:uid', placesControllers.getPlaceByUserId);
+router.get('/user/:uid', placesControllers.getPlacesByUserId);
 
 router.post('/', placesControllers.createPlace);
 
