15547655fe3a4c5c4ef6774e760f11602a942410
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-06-08 17:34:05 +0100

    created the placeSchema and the corresponding model

---
+++ models/place.js
@@ -0,0 +1,18 @@
+// CREATING A PLACE MODEL ----- A SCHEMA FOR PLACES TO BE STORED IN THE DB
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema; // Access the Schema method of mongoose package to create a Schema.
+
+const placeSchema = new Schema({ // The actual Schema to create a single place. Instantiate it to create a new JS object which contains the logic for the blueprint of the documents to be saved in the DB.
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    address: { type: String, required: true },
+    location: {
+        lat: { type: Number, required: true },
+        lng: { type: Number, required: true }
+    },
+    creator: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Place', ); // After we've created the Schema, we need to create the model as well. Special method in mongoose which returns a constructor function. First argument is the name of the model in this case "Place" and the second argument is the schema we want to refer to which in this case if "placeSchema"
