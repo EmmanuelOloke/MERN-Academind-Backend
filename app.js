94569acc4efd550699ad605698cba4defaaae153
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2025-06-25 23:44:22 +0100

    update app for containerization

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 const path = require('path'); // path module built into nodejs to serve files
+require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const placesRoutes = require('./routes/places-routes'); // Importing the configured route from places-routes.js ***** And this now is conviniently a middleware and use the middleware functions like app.use on it
+const placesRoutes = require('./routes/places-routes'); // Importing the configured route from places-routes.js ***** And this now is conveniently a middleware and use the middleware functions like app.use on it
 const userRoutes = require('./routes/users-routes');
 const HttpError = require('./models/http-error');
 
@@ -16,20 +17,23 @@ app.use(bodyParser.json()); // This will parse any incoming request on the body,
 app.use('/uploads/images', express.static(path.join('uploads', 'images'))); // Requests to the specified url will be handled by the express.static() middleware, built into express. It returns the requested file. It expects a path pointing to the folder from which you want to serve the files
 app.use(express.static(path.join('public')));
 
+// When you are serving both the frontend and the backend on the same host, you can omit the CORS header below
+app.use((req, res, next) => {
+  // Middleware to handle Cross-Origin Resource Sharing (CORS) error
+  res.setHeader('Access-Control-Allow-Origin', '*'); // This allows us to control which domain should have access to our requests, and setting it to '*' means we allow every domain to have accesss, thereby eliminating the CORS error.
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  ); // Specifying which headers the requests sent by the browser may have
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE'); // Controls which HTTP methods may be used on the Frontend
+  next();
+});
 
-// When you are serving both the frontend and the backend on the same host, you can ommit the CORS header below
-// app.use((req, res, next) => { // Middleware to handle Cross-Origin Resource Sharing (CORS) error
-//     res.setHeader('Access-Control-Allow-Origin', '*'); // This allows us to control which domain should have access to our requests, and setting it to '*' means we allow every domain to have accesss, thereby eliminating the CORS error.
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Specifying which headers the requests sent by the browser may have
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE'); // Controls which HTTP methods may be used on the Frontend
-//     next();
-// });
-
-app.use('/api/places', placesRoutes); // EpressJS will make sure to forward only Routes with paths beginning with /api/places to our places-routes.js configured file
+app.use('/api/places', placesRoutes); // ExpressJS will make sure to forward only Routes with paths beginning with /api/places to our places-routes.js configured file
 app.use('/api/users', userRoutes);
 
 app.use((req, res, next) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html')); // Method provided by express to send back a request made up of one file. For every unknown route we send back the index.html file, so that the frontend react routing can take care of any unknown url
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html')); // Method provided by express to send back a request made up of one file. For every unknown route we send back the index.html file, so that the frontend react routing can take care of any unknown url
 });
 
 // app.use((req, res, next) => {
@@ -37,22 +41,31 @@ app.use((req, res, next) => {
 //     throw error;
 // });
 
-app.use((error, req, res, next) => { // ExpressJS defualt error handler. Special middleware function with 4 parameters instead of 3 (error). ExpressJS treats middleware functions with 4 parameters as special/error handling functions. Only executed on requests that have an error attached to it.
-    if (req.file) { // Multer adds a file property to the req object, so we can check if we do have a file attached to the request, and roll back the saving process/delete the file if we don't
-        fs.unlink(req.file.path, (err) => { // We use the file system (fs) object to delete(unlink) the file. path is a property on the file object added by multer and it points to the file to be deleted. The callbac function will be triggered when the deletion is done
-            console.log(err);
-        });
-    }
+app.use((error, req, res, next) => {
+  // ExpressJS default error handler. Special middleware function with 4 parameters instead of 3 (error). ExpressJS treats middleware functions with 4 parameters as special/error handling functions. Only executed on requests that have an error attached to it.
+  if (req.file) {
+    // Multer adds a file property to the req object, so we can check if we do have a file attached to the request, and roll back the saving process/delete the file if we don't
+    fs.unlink(req.file.path, (err) => {
+      // We use the file system (fs) object to delete(unlink) the file. path is a property on the file object added by multer and it points to the file to be deleted. The callbac function will be triggered when the deletion is done
+      console.log(err);
+    });
+  }
 
-    if (res.headerSent) { // We check if a response has already been sent
-        return next(error);
-    }
-    res.status(error.code || 500); // We check if the code property on the error object is set. If it's not we fall back to the server error code 500.
-    res.json({ message: error.message || 'An unknown error occurred!' }); // Every error we send back from our API should have a message property, which the attached client can then use to show an error message to their user.
+  if (res.headerSent) {
+    // We check if a response has already been sent
+    return next(error);
+  }
+  res.status(error.code || 500); // We check if the code property on the error object is set. If it's not we fall back to the server error code 500.
+  res.json({ message: error.message || 'An unknown error occurred!' }); // Every error we send back from our API should have a message property, which the attached client can then use to show an error message to their user.
 });
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mern-academind-mongodb.4ruhkba.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`).then(() => {
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@academind-mongodb.r4go223.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  )
+  .then(() => {
     app.listen(process.env.PORT || 8000);
-}).catch(err => {
+  })
+  .catch((err) => {
     console.log(err);
-}); // .connect() returns a promise because connecting to the server is an asynchronous task
+  }); // .connect() returns a promise because connecting to the server is an asynchronous task
--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -5,110 +5,120 @@ const jwt = require('jsonwebtoken');
 const HttpError = require('../models/http-error');
 const User = require('../models/user'); // Importing the user model from user.js
 
-const getUsers = async(req, res, next) => {
-    let users;
-    try {
-        users = await User.find({}, '-password'); // With the .find() method here, we search the document stored in DB and find the users. By passing an empty object and the string '-password', we make sure the password field is not returned with other user info for security reasons.
-    } catch (err) {
-        const error = new HttpError('Fetching users failed, please try again later', 500);
-        return next(error);
-    }
-    res.json({ users: users.map(user => user.toObject({ getters: true })) }); // We're using the map() method here because .find() returns an array. Then turning each user to a default JS object so we can set getters: true to remove the default underscore in the returned id.
-}
+const getUsers = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find({}, '-password'); // With the .find() method here, we search the document stored in DB and find the users. By passing an empty object and the string '-password', we make sure the password field is not returned with other user info for security reasons.
+  } catch (err) {
+    const error = new HttpError('Fetching users failed, please try again later', 500);
+    return next(error);
+  }
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) }); // We're using the map() method here because .find() returns an array. Then turning each user to a default JS object so we can set getters: true to remove the default underscore in the returned id.
+};
 
-const signup = async(req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return next(new HttpError('Invalid field entry, please check your entries', 422));
-    }
+const signup = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(new HttpError('Invalid field entry, please check your entries', 422));
+  }
 
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-    let existingUser;
-    try {
-        existingUser = await User.findOne({ email: email }) // The findOne() here checks if the document matches the criteria passed into the method, hence checking if the email exists already
-    } catch (err) { // If the try block fails, we simply catch the error with a staus code of 500
-        const error = new HttpError('Signup failed, please try again', 500);
-        return next(error);
-    }
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email }); // The findOne() here checks if the document matches the criteria passed into the method, hence checking if the email exists already
+  } catch (err) {
+    // If the try block fails, we simply catch the error with a staus code of 500
+    const error = new HttpError('Signup failed, please try again', 500);
+    return next(error);
+  }
 
-    if (existingUser) {
-        const error = new HttpError('User exists already, please log in instead', 422);
-        return next(error);
-    }
+  if (existingUser) {
+    const error = new HttpError('User exists already, please log in instead', 422);
+    return next(error);
+  }
 
-    let hashedPassword;
-    try { // hash() returns a promise and since we're already in an async function, it makes sense to just use an await on it
-        hashedPassword = await bcrypt.hash(password, 12); // hash() takes two parameters, which are the password string to be hashed and a salting round number which determines how difficult the password will be to reverse-engineer
-    } catch (err) {
-        const error = new HttpError('Could not create user, please try again', 500);
-        return next(error);
-    }
-    const createdUser = new User({
-        name,
-        email,
-        image: req.file.path,
-        password: hashedPassword,
-        places: [] // New places will be added to the array by the user. Hence why we first initialize places to be an empty array
-    });
+  let hashedPassword;
+  try {
+    // hash() returns a promise and since we're already in an async function, it makes sense to just use an await on it
+    hashedPassword = await bcrypt.hash(password, 12); // hash() takes two parameters, which are the password string to be hashed and a salting round number which determines how difficult the password will be to reverse-engineer
+  } catch (err) {
+    const error = new HttpError('Could not create user, please try again', 500);
+    return next(error);
+  }
+  const createdUser = new User({
+    name,
+    email,
+    image: req.file.path,
+    password: hashedPassword,
+    places: [], // New places will be added to the array by the user. Hence why we first initialize places to be an empty array
+  });
 
-    try {
-        await createdUser.save(); // .save() is a method in mongoose that handles all the MongoDB logic you need to store a new collection in your DB. Returns a promise, so it's an asynchronous task
-    } catch (err) {
-        const error = new HttpError('Signing up failed, please try again', 500);
-        return next(error); // This stops code execution when/if we encounter an error
-    }
-    // At this point we know we've already stored a user in the database, so we know this is a valid user, so we can now generate a token for that user
-    let token;
-    try {
-        token = jwt.sign({ userId: createdUser.id, email: createdUser.email }, process.env.JWT_KEY, { expiresIn: '1h' }); // The sign method returns a string in the end, which will be the token. It takes two argument. The first arg is the payload of the token (the data you wanna encode into the token) which can be a string, an object or a buffer, here we passed an object containing the userId and email. The second argument is the private key string. The third argument is optional, check out the docs
-    } catch (err) {
-        const error = new HttpError('Signing up failed, please try again', 500);
-        return next(error);
-    }
-    // Eventually here we return the token and some user details, instead of the entire user object, because not all user data is required on the frontend, but that depends on the app being built
-    res.status(201).json({ userId: createdUser.Id, email: createdUser.email, token: token }); // Sending back the id of the user that was created, the email and the token that was generated.
-}
+  try {
+    await createdUser.save(); // .save() is a method in mongoose that handles all the MongoDB logic you need to store a new collection in your DB. Returns a promise, so it's an asynchronous task
+  } catch (err) {
+    const error = new HttpError('Signing up failed, please try again', 500);
+    return next(error); // This stops code execution when/if we encounter an error
+  }
+  // At this point we know we've already stored a user in the database, so we know this is a valid user, so we can now generate a token for that user
+  let token;
+  try {
+    token = jwt.sign({ userId: createdUser.id, email: createdUser.email }, process.env.JWT_KEY, {
+      expiresIn: '1h',
+    }); // The sign method returns a string in the end, which will be the token. It takes two argument. The first arg is the payload of the token (the data you wanna encode into the token) which can be a string, an object or a buffer, here we passed an object containing the userId and email. The second argument is the private key string. The third argument is optional, check out the docs
+  } catch (err) {
+    const error = new HttpError('Signing up failed, please try again', 500);
+    return next(error);
+  }
+  // Eventually here we return the token and some user details, instead of the entire user object, because not all user data is required on the frontend, but that depends on the app being built
+  res.status(201).json({ userId: createdUser.Id, email: createdUser.email, token: token }); // Sending back the id of the user that was created, the email and the token that was generated.
+};
 
-const login = async(req, res, next) => {
-    const { email, password } = req.body;
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
 
-    let existingUser;
-    try {
-        existingUser = await User.findOne({ email: email }) // The findOne() here checks if the document matches the criteria passed into the method, hence checking if the email exists already
-    } catch (err) { // If the try block fails, we simply catch the error
-        const error = new HttpError('Login failed, please try again', 500);
-        return next(error);
-    }
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email }); // The findOne() here checks if the document matches the criteria passed into the method, hence checking if the email exists already
+  } catch (err) {
+    // If the try block fails, we simply catch the error
+    const error = new HttpError('Login failed, please try again', 500);
+    return next(error);
+  }
 
-    if (!existingUser) {
-        const error = new HttpError('Invalid credentials, could not log you in', 403); // 403 is a forbidden error
-        return next(error);
-    }
+  if (!existingUser) {
+    const error = new HttpError('Invalid credentials, could not log you in', 403); // 403 is a forbidden error
+    return next(error);
+  }
 
-    isValidPassword = false;
-    try {
-        isValidPassword = await bcrypt.compare(password, existingUser.password); // The compare method provided by bcrypt compare the plain text password entered to the hashed password already saved in the db. It returns a promise which in the end yields a boolean.
-    } catch (err) {
-        const error = new HttpError('Could not log you in, please check your credentials and try again', 500);
-        return next(error);
-    }
+  isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password); // The compare method provided by bcrypt compare the plain text password entered to the hashed password already saved in the db. It returns a promise which in the end yields a boolean.
+  } catch (err) {
+    const error = new HttpError(
+      'Could not log you in, please check your credentials and try again',
+      500
+    );
+    return next(error);
+  }
 
-    if (!isValidPassword) {
-        const error = new HttpError('Invalid credentials, could not log you in', 401);
-        return next(error);
-    }
-    // We also generate the token on login. 
-    let token;
-    try {
-        token = jwt.sign({ userId: existingUser.id, email: existingUser.email }, process.env.JWT_KEY, { expiresIn: '1h' }); // We make sure to use the same private key as the one in signup route so we don't generate different tokens, so we can verify them on the backend
-    } catch (err) {
-        const error = new HttpError('Logging in failed, please try again', 500);
-        return next(error);
-    }
+  if (!isValidPassword) {
+    const error = new HttpError('Invalid credentials, could not log you in', 401);
+    return next(error);
+  }
+  // We also generate the token on login.
+  let token;
+  try {
+    token = jwt.sign({ userId: existingUser.id, email: existingUser.email }, process.env.JWT_KEY, {
+      expiresIn: '1h',
+    }); // We make sure to use the same private key as the one in signup route so we don't generate different tokens, so we can verify them on the backend
+  } catch (err) {
+    const error = new HttpError('Logging in failed, please try again', 500);
+    return next(error);
+  }
 
-    res.json({ userId: existingUser.id, email: existingUser.email, token: token }); // Sending back the userId, email and the token generated
-}
+  res.json({ userId: existingUser.id, email: existingUser.email, token: token }); // Sending back the userId, email and the token generated
+};
 
 exports.getUsers = getUsers;
 exports.signup = signup;
