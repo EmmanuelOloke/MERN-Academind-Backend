c724edf3be0caf944896605fb29ecc27440c419b
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2025-06-26 21:41:15 +0100

    update db connection and dockerfile

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@academind-mongodb.r4go223.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@academind-mongodb.r4go223.mongodb.net/`
   )
   .then(() => {
     app.listen(process.env.PORT || 8000);
